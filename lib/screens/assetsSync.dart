148d3232171ecd96d571909f5069c0ee6247c0ea
adc_100004713566_rocknio <adc_100004713566_rocknio@git.cloud.tencent.com>
2019-01-11 17:38:44 +0800

    同步页面上下分屏展示 增加待同步数量显示

--- lib/screens/assetsSync.dart
+++ lib/screens/assetsSync.dart
@@ -4,11 +4,13 @@ import 'package:photo_sync/models/assetModel.dart';
 
 class AssetsSyncPage extends StatelessWidget {
 	final List<AssetModel> assetsModel;
+	int _syncCount;
 
 	AssetsSyncPage({Key key, this.assetsModel}) : super(key: key);
 
 	@override
 	Widget build(BuildContext context) {
+		_syncCount = assetsModel.length;
 		return Container(
 			child: Column(
 			  children: <Widget>[
@@ -16,6 +18,18 @@ class AssetsSyncPage extends StatelessWidget {
 					  flex: 1,
 					  child: Container(
 						  decoration: BoxDecoration(color: Colors.blue),
+						  child: Row(
+							  children: <Widget>[
+							  	Expanded(
+									  child: Center(
+										  child: Text(
+												'$_syncCount',
+											  style: TextStyle(fontSize: 120.0, color: Colors.amberAccent, fontWeight: FontWeight.bold),
+										  ),
+									  ),
+								  )
+							  ],
+						  ),
 					  ),
 				  ),
 			  	Expanded(
