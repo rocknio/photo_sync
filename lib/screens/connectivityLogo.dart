eea2e54ef7610016579b8ee22cb15b2b5f5662a9
adc_100004713566_rocknio <adc_100004713566_rocknio@git.cloud.tencent.com>
2019-01-22 18:00:25 +0800

    图标颜色优化

--- lib/screens/connectivityLogo.dart
+++ lib/screens/connectivityLogo.dart
@@ -179,8 +179,8 @@ class Logo extends StatelessWidget {
 				padding: const EdgeInsets.symmetric(horizontal: 10.0),
 				child: Center(
 					child: connectType == 'ConnectivityResult.wifi'
-							? Icon(Icons.signal_wifi_4_bar, color: Colors.lightGreenAccent,)
-							: Icon(Icons.signal_cellular_4_bar, color: Colors.yellowAccent,),
+							? Icon(Icons.signal_wifi_4_bar, color: Colors.green,)
+							: Icon(Icons.signal_cellular_4_bar, color: Colors.blueGrey,),
 				),
 			);
 		} else {
